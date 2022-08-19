12ce1fe9955a3c85fc34f14f2ae9ec945707729d
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-08-19 16:04:42 +0400

    render form conditional

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import { useState } from "react";
 
 function Expenses(props) {
@@ -17,26 +17,13 @@ function Expenses(props) {
     changeFilteredYear(year);
   };
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onSelectYear={expenseYearHandler}
       />
-      {expensesContent}
+      <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,75 +1,56 @@
-import "./ExpenseForm.css";
-import { useState } from "react";
+import ShowExpenseForm from './ShowExpenseForm'
+import { useState } from 'react'
+import './ExpenseForm.css'
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  const [btnState, setBtnState] = useState(false)
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
-  };
+    setEnteredTitle(event.target.value)
+  }
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
-  };
+    setEnteredAmount(event.target.value)
+  }
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
-  };
+    setEnteredDate(event.target.value)
+  }
 
   const submitHandler = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const expenseData = {
-      title: enteredTitle,
-      amount: enteredAmount,
       date: new Date(enteredDate),
-    };
+      amount: enteredAmount,
+      title: enteredTitle,
+    }
 
-    props.onSaveExpenseData(expenseData);
-    setEnteredTitle("");
-    setEnteredAmount("");
-    setEnteredDate("");
-  };
+    props.onSaveExpenseData(expenseData)
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredDate('')
+
+    setBtnState(false)
+  }
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            value={enteredAmount}
-            min="0.01"
-            step="0.01"
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            value={enteredDate}
-            min="2020-04-30"
-            max="2022-12-31"
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add expense</button>
-      </div>
-    </form>
-  );
-};
+    <ShowExpenseForm
+      onChangeTitle={titleChangeHandler}
+      onChangeAmount={amountChangeHandler}
+      onChangeDate={dateChangeHandler}
+      onSubmit={submitHandler}
+      title={enteredTitle}
+      date={enteredDate}
+      amount={enteredAmount}
+      btnState={btnState}
+      setBtnState={setBtnState}
+    />
+  )
+}
 
-export default ExpenseForm;
+export default ExpenseForm
